4e1cfd35a7b31e42a8a7e4e593aff507fe518247
Andrew Garcia <garcia.gtr@gmail.com>
2018-07-01 14:23:13 -0400

    cleaned up tech-anlsys.py code for easier comprehension, changed stock opt. examples

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # quant-python
 ## Quantitative finance with python
 
-Published code herein, _tech_anal.py_ is a script with adapted codes from Ives Hilpish's book _*Python for Finance*_ which show simple technical analysis algorithms for stocks. The figures showed herein are the results of an optimization program which integrate tech_anal.py to a [secret] Monte Carlo algorithm which I will not push. Algorithms are backtesting of stock data and could potentially be used to optimize returns.
+Published code herein, _tech_anal.py_ is an *ADAPTED* script from the codes from Ives Hilpish's book _*Python for Finance*_ which show simple technical analysis algorithms for stocks.
+
+The figures showed herein are the results of an optimization script _vicious_gnauga.py_ which integrates _tech_anal.py_ to a [secret] Monte Carlo algorithm (not pushed here; confidential) which finds optimal backtesting parameters for a certain stock, showing optimal buy-hold-sell regimes for the historical period (step-wise line in red) through limited knowledge of future values.
+
+![Vicious Gnauga, (c) kingdomofloathing.com, KoL](http://images.kingdomofloathing.com/adventureimages/gnauga.gif)
--- tech-anlsys.py
+++ tech-anlsys.py
@@ -11,82 +11,78 @@ import pandas as pd
 import pandas_datareader.data as web
 import datetime as dt
 
-import comm_pars
-'''comm_pars.py: returns ticker name (as a string) of commodity, 
-rolling means 1 and 2 and SD (as numbers) from vector def
-e.g. def Netflix:
-        #return [TICKER NAME, interval for moving average 1," " moving average 2 , SD('tolerance')]
-        return ['NFLX',42,365,5]
-will not push this stock_params.py; personal stock info'''
+
+'''# ==================== STOCK PARAMETERS GO HERE ========================='''
+
+ticker_name='FNKO'
+
+'moving averages to compare against'
+roll_mean1 = 5
+roll_mean2 = 21
+
+'tolerance for moving avg. comparison / regime determination'
+SD = 1
+
+'''# ======================================================================='''
+
+    
+'beginning date of data collection'
+start = dt.datetime(2015, 1, 1)
+'end date'
+end = dt.datetime.now()
 
 
-def qtech( commodity = comm_pars.Funko() ):
-    
-    'beginning date of data collection'
-    start = dt.datetime(2015, 1, 1)
-    'end date'
-    end = dt.datetime.now()
-    
-    name, roll_mean1,roll_mean2, SD = commodity
-    'or if inputting values  manually ; uncomment line 31, comment out line 29:'
-#    name, roll_mean1,roll_mean2, SD = 'TSLA', 21, 24, 5
-    
-    
-    hist_data = web.DataReader(name, 'morningstar', start, end)
-    
-    print(hist_data)
+hist_data = web.DataReader(ticker_name, 'morningstar', start, end)
 
-    '''for data not present in NASDAQ (i.e. coins) need to uncomment lines / make 
-    code modifications''' 
-#    hist_data = pd.read_csv(name+'.csv')
+'''for data not present in NASDAQ (i.e. coins) need to uncomment lines below / make 
+code modifications''' 
+#    hist_data = pd.read_csv(ticker_name+'.csv')
 #    hist_data.info()
-    
-#    hist_data[['date','close']].plot(title = name, x='date', grid=False, figsize=(8, 5))
-    hist_data[['Close']].plot(title = '{} Historical Data'.format(name), grid=False, figsize=(8, 5))
 
-    
+#    hist_data[['date','close']].plot(title = ticker_name, x='date', grid=False, figsize=(8, 5))
+
+print(hist_data)
+
+hist_data[['Close']].plot(title = '{} Historical Data'.format(ticker_name), grid=False, figsize=(8, 5))
+
+
    
-    ''' Historical data with moving averages '''
-    
-    hist_data[str(roll_mean1)+'d'] = np.round(pd.rolling_mean(hist_data['Close'], window=roll_mean1), 2)
-    hist_data[str(roll_mean2)+'d'] = np.round(pd.rolling_mean(hist_data['Close'], window=roll_mean2), 2)
-    
-    hist_data[['Close', str(roll_mean1)+'d', str(roll_mean2)+'d']].tail()
-    
+''' Historical data with moving averages '''
+
+hist_data[str(roll_mean1)+'d'] = np.round(pd.rolling_mean(hist_data['Close'], window=roll_mean1), 2)
+hist_data[str(roll_mean2)+'d'] = np.round(pd.rolling_mean(hist_data['Close'], window=roll_mean2), 2)
+
+hist_data[['Close', str(roll_mean1)+'d', str(roll_mean2)+'d']].tail()
+
 #    hist_data[['date','close', str(roll_mean1)+'d', str(roll_mean2)+'d']].plot(x='date',grid=False, figsize=(8, 5))
-    hist_data[['Close', str(roll_mean1)+'d', str(roll_mean2)+'d']].plot(grid=False, figsize=(8, 5))
+hist_data[['Close', str(roll_mean1)+'d', str(roll_mean2)+'d']].plot(grid=False, figsize=(8, 5))
 
-    
-    '''regime det. '''
-    
-    hist_data['42-252'] = hist_data[str(roll_mean1)+'d'] - hist_data[str(roll_mean2)+'d']
-    
-    hist_data['42-252'].tail()
-    
-    hist_data['regime'] = np.where(hist_data['42-252'] > SD, 1, 0)
-    hist_data['regime'] = np.where(hist_data['42-252'] < -SD, -1, hist_data['regime'])
-    hist_data['regime'].value_counts()
-    hist_data['regplot']=max(hist_data['Close'])*(2+hist_data['regime'])/8 
-    
-    hist_data['regplot'].plot(title= '{} technical analysis'.format(name),  grid=False,lw=1.5)
-    #plt.ylim([-1.1, 1.1])
-    
-    '''Market / Strategy comparison '''
-    
-    hist_data['market'] = np.log(hist_data['Close'] / hist_data['Close'].shift(1))
-    hist_data['strategy'] =  hist_data['market'] *(1+hist_data['regime'].shift(1))
-    
-    hist_data[['market','strategy']].cumsum().apply(np.exp).\
-    plot(title='{} Market v. Strategy Return comparison'.format(name),grid=False, figsize=(8, 5))
-    
-    X=hist_data['strategy'].cumsum().iloc[-1]-hist_data['market'].cumsum().iloc[-1]
-    
-    print('final data pt. difference b/t market & strat: ',X)
+
+''' buy / sell / hold regime determination '''
+
+hist_data['42-252'] = hist_data[str(roll_mean1)+'d'] - hist_data[str(roll_mean2)+'d']
+
+hist_data['42-252'].tail()
+
+hist_data['regime'] = np.where(hist_data['42-252'] > SD, 1, 0)
+hist_data['regime'] = np.where(hist_data['42-252'] < -SD, -1, hist_data['regime'])
+hist_data['regime'].value_counts()
+hist_data['regplot']=max(hist_data['Close'])*(2+hist_data['regime'])/8 
+
+hist_data['regplot'].plot(title= '{} technical analysis'.format(ticker_name),  grid=False,lw=1.5)
+#plt.ylim([-1.1, 1.1])
+
+'''Market / Strategy comparison '''
+
+hist_data['market'] = np.log(hist_data['Close'] / hist_data['Close'].shift(1))
+hist_data['strategy'] =  hist_data['market'] *(1+hist_data['regime'].shift(1))
+
+hist_data[['market','strategy']].cumsum().apply(np.exp).\
+plot(title='{} Market v. Strategy Return comparison'.format(ticker_name),grid=False, figsize=(8, 5))
+
+X=hist_data['strategy'].cumsum().iloc[-1]-hist_data['market'].cumsum().iloc[-1]
+
+print('final data pt. difference b/t market & strat: ',X)
 #    return X
-
-qtech()
-
-
-
 
 
